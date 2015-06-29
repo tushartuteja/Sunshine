21f97aae597797265ae31bdf3dc27ba4314d15a1
tushartuteja <ttuteja@gmail.com>
2015-06-29 13:44:26 +0530

    content providers testing commit

---
+++ app/src/androidTest/java/com/example/tushartuteja/sunshine/app/TestProvider.java
@@ -0,0 +1,102 @@
+package com.example.tushartuteja.sunshine.app;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import com.example.tushartuteja.sunshine.app.data.WeatherContract;
+import com.example.tushartuteja.sunshine.app.data.WeatherDbHelper;
+
+/**
+ * Created by tushartuteja on 26/06/15.
+ */
+public class TestProvider extends AndroidTestCase {
+    public static final String LOG_TAG ="TEST-DB";
+
+
+    public void testDeleteDb() throws Throwable{
+        mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
+
+    }
+
+    public void testInsertReadDb(){
+        String testName = "North Pole";
+        String testLocationSetting = "99705";
+        Double testLatitude = 64.772;
+        Double testLongitude = -147.355;
+
+        WeatherDbHelper weatherDbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase db = weatherDbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(WeatherContract.LocationEntry.COLUMN_CITY_NAME, testName);
+        contentValues.put(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
+        contentValues.put(WeatherContract.LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        contentValues.put(WeatherContract.LocationEntry.COLUMN_COORD_LONG, testLongitude);
+
+        long locationRowEntry = db.insert(WeatherContract.LocationEntry.TABLE_NAME,null, contentValues);
+
+        assertTrue(locationRowEntry != -1);
+
+        Log.d(LOG_TAG, "Insert Row Db: " + locationRowEntry);
+
+
+        String[] columns = {
+                WeatherContract.LocationEntry._ID,
+                WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+                WeatherContract.LocationEntry.COLUMN_CITY_NAME,
+                WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+                WeatherContract.LocationEntry.COLUMN_COORD_LONG
+
+        };
+
+
+        Cursor cursor = db.query(WeatherContract.LocationEntry.TABLE_NAME, columns,null,null,null,null,null);
+
+        if (cursor.moveToFirst()){
+            int locationIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING);
+            String location = cursor.getString(locationIndex);
+
+            int latIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_COORD_LAT);
+            double latitude = cursor.getDouble(latIndex);
+
+            int longIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_COORD_LONG);
+            double longitude = cursor.getDouble(longIndex);
+
+            int nameIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_CITY_NAME);
+            String name = cursor.getString(nameIndex);
+
+            assertEquals(location, testLocationSetting);
+            assertEquals(testName, name);
+            assertEquals(testLatitude, latitude);
+            assertEquals(testLongitude, longitude);
+
+
+        }else{
+            fail("No values returned");
+        }
+    }
+
+
+    public void testGetType(){
+        String type = mContext.getContentResolver().getType(WeatherContract.WeatherEntry.CONTENT_URI);
+        assertEquals(WeatherContract.WeatherEntry.CONTENT_TYPE, type);
+
+        String testLocation = "94074";
+        type = mContext.getContentResolver().getType(WeatherContract.WeatherEntry.buildWeatherLocation(testLocation));
+        assertEquals(WeatherContract.WeatherEntry.CONTENT_TYPE, type);
+
+        String testDate = "20140612";
+        type = mContext.getContentResolver().getType(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(testLocation,testDate));
+        assertEquals(WeatherContract.WeatherEntry.CONTENT_ITEM_TYPE, type);
+
+        type = mContext.getContentResolver().getType(WeatherContract.LocationEntry.CONTENT_URI);
+        assertEquals(WeatherContract.LocationEntry.CONTENT_TYPE, type);
+
+        type = mContext.getContentResolver( ).getType(WeatherContract.LocationEntry.buildLocationUri(1L));
+        assertEquals(WeatherContract.LocationEntry.CONTENT_ITEM_TYPE, type);
+
+    }
+}
--- app/src/main/java/com/example/tushartuteja/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/tushartuteja/sunshine/app/data/WeatherContract.java
@@ -1,5 +1,7 @@
 package com.example.tushartuteja.sunshine.app.data;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,7 +9,17 @@ import android.provider.BaseColumns;
  */
 public class WeatherContract  {
 
+    public static final String CONTENT_AUTHORITY = "com.example.tushartuteja.sunshine.app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+
+    public static final String PATH_WEATHER = "weather";
+    public static final String PATH_LOCATION ="location";
+
     public static final class WeatherEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + PATH_WEATHER;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + PATH_WEATHER;
+
         public static final String TABLE_NAME = "weather";
         public static final String COLUMN_LOC_KEY = "location_id";
         public static final String COLUMN_DATETEXT = "date";
@@ -19,14 +31,49 @@ public class WeatherContract  {
         public static final String COLUMN_PRESSURE = "pressure";
         public static final String COLUMN_WIND_SPEED = "wind";
         public static final String COLUMN_DEGREES = "degrees";
+
+        public static Uri buildWeatherUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(String locationSetting, String startDate){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).appendQueryParameter(COLUMN_DATETEXT, startDate).build();
+        }
+
+        public static Uri buildWeatherLocationWithDate(String locationSettting, String startDate){
+            return CONTENT_URI.buildUpon().appendPath(locationSettting).appendPath(startDate).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri){
+            return uri.getPathSegments().get(1);
+        }
+
+        public static String getDateFromUri(Uri uri){
+            return uri.getPathSegments().get(2);
+        }
+        public static String getStartDateFromUri(Uri uri){
+            return uri.getQueryParameter(COLUMN_DATETEXT);
+        }
     }
     public static final class LocationEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + PATH_LOCATION;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + PATH_LOCATION;
 
         public static final String TABLE_NAME = "location";
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
         public static final String COLUMN_CITY_NAME = "city_name";
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static Uri buildLocationUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
 
     }
 
